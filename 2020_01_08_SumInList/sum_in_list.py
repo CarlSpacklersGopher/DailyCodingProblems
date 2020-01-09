003ee64a75e4ccda85e31244ddd09bdb426c736f
unknown <garrett.d.manship@gmail.com>
2020-01-09 00:18:31 -0600

    Refactored is_sum_in_list to traverse the list one time.

--- 2020_01_08_SumInList/sum_in_list.py
+++ 2020_01_08_SumInList/sum_in_list.py
@@ -5,12 +5,17 @@ For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 
 Bonus: Can you do this in one pass?
 """
+
 def is_sum_in_list(list_of_numbers, k):
-    # Brute force.
-    for outer_loop_idx in range(len(list_of_numbers)):
-        number1 = list_of_numbers[outer_loop_idx]
-        for inner_loop_idx in range(outer_loop_idx + 1, len(list_of_numbers)):
-            number2 = list_of_numbers[inner_loop_idx]
-            if number1 + number2 == k:
-                return True
+    # Can store all numbers already visited and look them up from this set in constant time.
+    numbers_already_visited = set()
+    for number in list_of_numbers:
+        # number1 + number2 = k
+        # Since we have number1 and k, we can calculate the necessary value of number2.
+        number2 = k - number
+        if number2 in numbers_already_visited:
+            # If we've already seen number2, current value of number + number2 = k
+            return True
+
+        numbers_already_visited.add(number) # Store in set for following iterations
     return False
