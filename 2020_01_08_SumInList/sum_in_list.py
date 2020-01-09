88e16a1cd19e1453a7d98879dce75c8ffe4f969e
unknown <garrett.d.manship@gmail.com>
2020-01-08 23:46:26 -0600

    implemented brute force method with nested loops

--- 2020_01_08_SumInList/sum_in_list.py
+++ 2020_01_08_SumInList/sum_in_list.py
@@ -1,2 +1,16 @@
-def is_sum_in_list(list_of_numbers, desired_sum):
-    pass
+"""
+PROBLEM STATEMENT:
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+Bonus: Can you do this in one pass?
+"""
+def is_sum_in_list(list_of_numbers, k):
+    # Brute force.
+    for outer_loop_idx in range(len(list_of_numbers)):
+        number1 = list_of_numbers[outer_loop_idx]
+        for inner_loop_idx in range(outer_loop_idx + 1, len(list_of_numbers)):
+            number2 = list_of_numbers[inner_loop_idx]
+            if number1 + number2 == k:
+                return True
+    return False
